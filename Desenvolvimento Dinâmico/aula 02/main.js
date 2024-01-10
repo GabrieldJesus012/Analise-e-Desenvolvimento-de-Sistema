69baa961ac77f4d14661b1f5d0963ece4c9bd067
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2024-01-10 18:30:12 -0300

    att

---
+++ Desenvolvimento Dinâmico/aula 02/main.js
@@ -0,0 +1,26 @@
+console.log("testando")
+var a = 10;
+var b = 20;
+var total= a + b;
+console.log(total);
+
+var nomepessoa = "Gabriel";
+var NOMEPESSOA = "Paula";
+console.log (NOMEPESSOA);
+var var1 = 1,
+    var2= 2,
+    var3= 3;
+
+var variavel1;
+variavel1=23
+console.log(variavel1);
+
+
+let book=10;
+book=20;
+    console.log(book);
+
+
+const valor1=100;
+valor1=200;
+console.log(valor1);
