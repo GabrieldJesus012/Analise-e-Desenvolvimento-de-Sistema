f6f8556e8e723f681e1569e1e46b6ea7c6a779d6
Gabriel de Jesus <gabrieldjesus055@outlook.com>
2024-04-29 20:49:45 -0300

    att

---
+++ 1º Trimestre/Logica de Programação/MeuSegundoProgramaJava.java
@@ -0,0 +1,21 @@
+//salvar como MeuSegundoProgramaJava.java
+
+//nome da classe
+class MeuSegundoProgramaJava
+{
+   //módulo principal com a entrada pela linha de comando
+ public static void main (string entrada[])
+ {
+   //declaração de variáveis
+     int n1, n2, soma;
+    
+   // entrada de dados
+   n1= Integer.parseInt(entrada [0]);
+   n2= Integer.parseInt(entrada [1]);
+   //procesamento
+   soma = n1 + n2;
+   //saida de resultados
+   System.out.println(n1+ " + " + n2 + " = " + soma);
+   System.exit(0);
+ }
+}
