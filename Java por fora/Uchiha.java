451502edfa5ed80b8773a99a5ce8eccae9604f3e
GabrieldJesus012 <gabrieldjesus055@outlook.com>
2024-10-25 20:18:59 -0300

    at

--- Java por fora/Uchiha.java
+++
@@ -1,6 +0,0 @@
-
-public class Uchiha extends Ninja{
-	public void ShariganAtivado() {
-		System.out.println("Meu nome é " + getNome() + ". O Sharigan Ativo, eu sou um Uchiha!");
-	}
-}
--- Java por fora/Uzumaki.java
+++
@@ -1,6 +0,0 @@
-
-public class Uzumaki extends Ninja {
-	public void ModoSabioAtivado() {
-		System.out.println("Meu " + getNome() + ". E eu ativei o modo Sabio");
-	}
-}
