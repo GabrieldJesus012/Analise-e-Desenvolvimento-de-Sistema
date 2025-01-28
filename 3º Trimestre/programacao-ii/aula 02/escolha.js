8ad748771bcf3a21f96704108e676f93f4fb9b89
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-01-28 20:23:31 -0300

    nodejs

---
+++ 3º Trimestre/programacao-ii/aula 02/escolha.js
@@ -0,0 +1,23 @@
+import{createServer} from 'http';
+import {parse} from 'url';
+import {readFile} from 'fs';
+
+createServer(
+    function(req, res){
+        let q = parse(req.url, true);
+        let filename = './aula 02/' + q.pathname;
+        console.log(q);
+        console.log(filename);
+        
+        readFile(filename, function(err, data){
+            console.log(err);
+            if(err){
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end('404 Not Found');
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
+).listen(8080)
---
+++ 3º Trimestre/programacao-ii/aula 02/path.js
@@ -0,0 +1,3 @@
+import { basename} from 'path';
+let nomeArquivo = basename('C:/Users/gabri/OneDrive/Documentos/GitHub/nodejs/aula 02/teste.txt');
+console.log(nomeArquivo);
