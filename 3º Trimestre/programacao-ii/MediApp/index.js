5934b430acae53315ac6920cddbec60c7ee64a5c
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-01-28 21:24:50 -0300

    update  programacao II

---
+++ 3º Trimestre/programacao-ii/MediApp/index.js
@@ -0,0 +1,11 @@
+import {express} from 'express';
+import {pkg} from 'body-parser';
+import { routes} from './routes/routes.js'
+
+const app = express();
+const {json,urlencoded} = pkg;
+app.use(json());
+app.use(urlencoded({extended: true}));
+app.listen(3000,function(){
+    console.log('Servidor rodando na porta 3000');
+});
